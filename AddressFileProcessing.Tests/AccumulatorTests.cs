1fdc995cbdca7f9cd0b8a77526f9da883760224c
thierryx96 <thierry.rais@gmail.com>
2017-02-18 14:59:49 +1000

    add missing renamed files

---
+++ AddressFileProcessing.Tests/AccumulatorTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Linq;
+using AddressFileProcessing.Models;
+using AddressFileProcessing.Storage;
+using NUnit.Framework;
+
+namespace AddressFileProcessing.Tests
+{
+    [TestFixture]
+    public class AccumulatorTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            _accumulator = new Accumulator();
+        }
+
+        private Accumulator _accumulator;
+
+        [Test]
+        public void Append_SingleEntry_ShouldReturnTwoNamesAndOneAddresses()
+        {
+            // arrange
+            var inputSet = new []
+            {
+                new Person("jo", "valjean", "champ-elysees", "40")
+            };
+
+            _accumulator.Append(inputSet);
+            // act
+            var actualAddresses = _accumulator.GetOrderedAddresses();
+            var actualNames = _accumulator.GetNameFrequencies();
+
+            // assert
+            Assert.AreEqual(1, actualAddresses.Count);
+            Assert.AreEqual(2, actualNames.Count);
+        }
+
+        [Test]
+        public void Append_SingleEntryWithSameName_ShouldAggregateIntoOneName()
+        {
+            // arrange
+            var inputSet = new[]
+            {
+                new Person("jo", "valjean", "champ-elysees", "40")
+            };
+
+            _accumulator.Append(inputSet);
+            // act
+            var actualAddresses = _accumulator.GetOrderedAddresses();
+            var actualNames = _accumulator.GetNameFrequencies();
+
+            // assert
+            Assert.AreEqual(1, actualAddresses.Count);
+            Assert.AreEqual(1, actualNames.Count);
+            Assert.AreEqual(2, actualNames.Single().Frequency);
+
+        }
+
+
+        [Test]
+        public void Append_SameNameEntry_ShouldAggregateFrequency()
+        {
+            // arrange
+            var inputSet = new []
+            {
+                new Person("jo", "valjean", "champ-elysees", "40"),
+                new Person("jo", "valjean", "champ-elysees", "1")
+            };
+
+            // act
+            _accumulator.Append(inputSet);
+
+            var actualAddresses = _accumulator.GetOrderedAddresses();
+            var actualNames = _accumulator.GetNameFrequencies();
+
+            // assert
+            Assert.AreEqual(2, actualAddresses.Count);
+            Assert.AreEqual(2, actualNames.Count);
+            Assert.AreEqual(2, actualNames.First().Frequency);
+            Assert.AreEqual(2, actualNames.Last().Frequency);
+        }
+
+
+        [Test]
+        public void Append_WithSameNameEntry_ShouldAggregateSingleNameFrequency()
+        {
+            // arrange
+            var inputSet = new[]
+            {
+                new Person("jean", "jean", "bee lane", "1"),
+                new Person("jean", "jean", "apple st", "40"),
+                new Person("jean", "jean", "cat way", "4")
+            };
+
+            var expectedAddresses = new[]
+            {
+                new Address("apple st", "40"),
+                new Address("bee lane", "1"),
+                new Address("cat way", "4")
+            };
+
+            // act           
+            _accumulator.Append(inputSet);
+            var actualAddresses = _accumulator.GetOrderedAddresses();
+
+            // assert
+            CollectionAssert.AreEqual(expectedAddresses, actualAddresses);
+        }
+
+        [Test]
+        public void Append_WithManyEntry_ShouldOrderNamesByFrequencyAndThenByFirstName()
+        {
+            // arrange
+            var inputSet = new[]
+            {
+                new Person("jean",    "valjean", "bee lane", "1"),
+                new Person("charles", "valjean", "apple st", "40"),
+                new Person("valjean", "dupont",  "cat way",  "1"),
+                new Person("boris",   "charles", "cat way",  "2"),
+                new Person("boris",   "gates",   "cat way",  "3")
+            };
+
+            var expectedNames = new[]
+            {
+                new NameFrequency("valjean", 3),
+                new NameFrequency("boris", 2),
+                new NameFrequency("charles", 2),
+                new NameFrequency("dupont", 1),
+                new NameFrequency("gates", 1),
+                new NameFrequency("jean", 1)
+            };
+
+            // act           
+            _accumulator.Append(inputSet);
+            var actualNames = _accumulator.GetNameFrequencies();
+
+            // assert
+            Assert.AreEqual(expectedNames, actualNames);
+        }
+    }
+}
---
+++ AddressFileProcessing.Tests/PersonCsvEntryParserTests.cs
@@ -0,0 +1,62 @@
+﻿using AddressFileProcessing.Parsing;
+using NUnit.Framework;
+
+namespace AddressFileProcessing.Tests
+{
+    [TestFixture]
+    public class PersonAddressCsvEntryParserTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            _parser = new PersonCsvEntryParser();
+        }
+
+        private PersonCsvEntryParser _parser;
+
+        [Test]
+        public void Parse_WithInvalidAddressField_ShouldThrowException()
+        {
+            // arrange
+            var input = "OnlyOneField";
+
+            // act & assert
+            Assert.Throws<ParsingException>(() => _parser.Parse(input),
+                 $"Error when parsing input: '{input}'. Field index: '{2}' has erroneous value: '{input}'.");
+        }
+
+
+        [Test]
+        public void Parse_WithMissingFields_ShouldThrowException()
+        {
+            // arrange
+            var input = "OnlyOneField";
+
+            // act & assert
+            Assert.Throws<ParsingException>(() => _parser.Parse(input),
+                $"Not enough fields found when parsing input: '{input}'. Expected: '{3}', actual: '{1}'.");
+        }
+
+
+        [Test]
+        public void Parse_WithValidAddress_ShouldReturnParsedAddress()
+        {
+            // arrange
+            var expectedFirstName = "Jimmy";
+            var expectedLastName = "Smith";
+            var expectedStreetName = "Long Lane";
+            var expectedStreetNumber = "102";
+
+            var input = "Jimmy,Smith,102 Long Lane,29384857";
+
+            // act 
+            var actualPerson = _parser.Parse(input);
+
+            // assert
+            Assert.AreEqual(expectedFirstName, actualPerson.FirstName);
+            Assert.AreEqual(expectedLastName, actualPerson.LastName);
+            Assert.AreEqual(expectedStreetNumber, actualPerson.Address.StreetNumber);
+            Assert.AreEqual(expectedStreetName, actualPerson.Address.StreetName);
+        }
+    }
+}
